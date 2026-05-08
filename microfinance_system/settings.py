d9f7cff7bfaa79c0807dd73da84f4597528f5f2f
Selim Ahmed <selim174662@gmail.com>
2026-05-08 11:39:49 +0600

    Change the project structure for make project simple in role based authentication

--- microfinance_system/settings.py
+++ microfinance_system/settings.py
@@ -37,11 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
     'products',
     'groups',
     'loans',
-    'dashboard',
 ]
 
 MIDDLEWARE = [
