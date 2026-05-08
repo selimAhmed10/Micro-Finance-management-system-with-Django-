75f3bb586ccee7e26b3aa44429b27e397e9f581f
Selim Ahmed <selim174662@gmail.com>
2026-05-08 10:13:45 +0600

    Complete the products crub operation with the simple templetes

--- products/models.py
+++ products/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
 class Product(models.Model):
-    
-    name=models.CharField(max_length=50)
-    amount=models.DecimalField(max_digits=5,decimal_places=2)
-    interest_rate=models.DecimalField(max_digits=2,decimal_places=2)
-    total_installment=models.IntegerField()
-    per_install_amount=models.DecimalField(max_digits=5,decimal_places=2)
+    name = models.CharField(max_length=50)
+    amount = models.FloatField()
+    interest_rate = models.FloatField()
+    total_installment = models.IntegerField()
+    per_install_amount = models.FloatField()
     
     def __str__(self):
         return self.name
--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.view_all_products),
+    path('', views.view_all_products, name='view_all_products'), 
+    path('create/', views.create_product, name='create_product'),
+    path('update/<int:id>/', views.update_product, name='update_product'),
+    path('delete/<int:id>/', views.delete_product, name='delete_product'),
 ]
