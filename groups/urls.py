221b8cd4c4e58f0b71299f9fcf37aa7a2dca61b3
Selim Ahmed <selim174662@gmail.com>
2026-05-08 23:59:27 +0600

    Now succesfully done the group management by the admin where he can select existing officer

--- groups/urls.py
+++ groups/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.view_group),
-    path('create_group',views.create_group),
-    path('edit/<int id>/',views.modify_group),
-    path('delete/<int:id>',views.delete_group),
+    path('create/',views.create_group),
+    path('edit/<int:id>/',views.modify_group),
+    path('delete/<int:id>/',views.delete_group),
 ]
--- groups/views.py
+++ groups/views.py
@@ -1,47 +1,61 @@
 from django.shortcuts import render
-from django.shortcuts import redirect,get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
 from .models import Group
 from django.contrib import messages
-from .models import Officer
+from officer.models import Officer
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def view_group(request):
-    if not hasattr(request.user,'admin'):
+    if not hasattr(request.user, 'admin'):
         return redirect('/')
-    groups=Group.objects.all()
-    return render(request,'groups/list.html',{'groups':groups})
-
-
+    groups = Group.objects.all()
+    return render(request, 'groups/list.html', {'groups': groups})
 
 def create_group(request):
-    if request.method=='POST':
-        Group.objects.create(
-        name=request.POST.get['name'],
-        location=request.POST.get['location'],
+    if request.method == 'POST':
+        name = request.POST['name']
+        location = request.POST['location']
+        officer_id = request.POST.get('officer')
+        
+        group = Group.objects.create(
+            name=name,
+            location=location
         )
-        messages.success(request,"Group created successfull")
-        return redirect('view_group')
-    return render(request,'groups/form.html')
-
-
-
-def modify_group(request,id):
-    groups=get_object_or_404(Group,id=id) 
-    if request.method=='POST':
-        groups.name=request.POST.get['name']
-        groups.location=request.POST.get['location']
-        groups.save()
-        messages.success(request,"GGroup update successfully")
-        return redirect('view_group')
-    return render(request,'groups/form.html',{'groups':groups})
-
-
-
-def delete_group(request,id):
-    groups=get_object_or_404(Group,id=id)
-    groups.delete()
-    messages.success(request,"Group deleted")
-    return redirect('view_group')
-      
+        if officer_id:
+            officer = get_object_or_404(Officer, id=officer_id)
+            group.officer = officer
+            group.save()
+        
+        messages.success(request, "Group created")
+        return redirect('/groups/')  
     
+    officers = Officer.objects.all()
+    return render(request, 'groups/form.html', {'officers': officers})
+
+def modify_group(request, id):
+    group = get_object_or_404(Group, id=id)
+    
+    if request.method == 'POST':
+        group.name = request.POST['name']
+        group.location = request.POST['location']
+        
+        officer_id = request.POST.get('officer')
+        if officer_id:
+            officer = get_object_or_404(Officer, id=officer_id)
+            group.officer = officer
+        else:
+            group.officer = None
+        
+        group.save()
+        messages.success(request, "Group updated successfully")
+        return redirect('/groups/')  
+    
+    officers = Officer.objects.all()
+    return render(request, 'groups/form.html', {'group': group, 'officers': officers})
+
+def delete_group(request, id):
+    group = get_object_or_404(Group, id=id)
+    group.delete()
+    messages.success(request, "Group deleted")
+    return redirect('/groups/') 
