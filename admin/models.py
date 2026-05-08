ae915ef4557ad74b7edb64256325a5e1a53e8fbe
Selim Ahmed <selim174662@gmail.com>
2026-05-08 13:07:18 +0600

    create the admin models class with necessary attribute and migrate it

--- admin/models.py
+++ admin/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Admin(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=50)
+    phone_num=models.CharField(max_length=14)
+    degisnation=models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
