778cd6742e7d5b6abf4215990dcab9517faefcb7
Selim Ahmed <selim174662@gmail.com>
2026-05-08 08:29:05 +0600

    Added the products updateb,create,delete function in views for doing CRUD operation first

--- products/views.py
+++ products/views.py
@@ -1,11 +1,42 @@
-from django.shortcuts import render
-from django.shortcuts import redirect,get_list_or_404
+from django.shortcuts import get_object_or_404, render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from .models import Product
 
 
 def view_all_products(request):
-    products=Product.objects.all()
-    return render(request,'products/list.html',{'products':products})
+    product=Product.objects.all()
+    return render(request,'products/list.html',{'product':product})
+
+def create_product(request):
+    if request.method=='POST':
+        Product.objects.create(
+            name=request.POST['name'],
+            amount=request.POST['amount'],
+            interest_rate=request.POST['interest_rate'],
+            total_installment=request.POST['total_installment'],
+            per_install_amount=request.POST['per_install_amount']  
+        )
+        
+        return redirect('view_all_products')
+    return render (request,'products/form.html')
+
+def update_product(request,id):
+    product=get_object_or_404(Product,id=id)
+    if request.method=='POST':
+        product.name=request.POST['name']
+        product.amount=request.POST['amount']
+        product.interest_rate=request.POST['interest_rate']
+        product.total_installment=request.POST['total_installment']
+        product.per_install_amount=request.POST['per_install_amount']  
+        product.save()
+        return redirect('view_all_products')
+    return render(request,'products/form.html',{'product':product})
+
+def delete_product(request,id):
+    product=get_object_or_404(Product,id=id)
+    product.delete()
+    return redirect('view_all_products')
+        
 
 
