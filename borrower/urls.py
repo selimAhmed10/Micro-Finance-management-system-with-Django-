dd7f4f1920d06109a90beb3c9870f7f0a20e51d7
Selim Ahmed <selim174662@gmail.com>
2026-05-09 10:37:27 +0600

    Add the member crud operation like the product,member with the list and form templetes to do crud operations

--- borrower/urls.py
+++ borrower/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard),
+    path('view/', views.view_borrower),
+    path('create/', views.borrower_create),
+    path('edit/<int:id>/', views.borrower_edit),
+    path('delete/<int:id>', views.borrower_delete),
 ]
